840dc74205278ab1570487aaa83295472b641a9d
teachyyou <kudriavtsiev_2004@mail.ru>
2024-07-15 11:28:12 +0300

    Fixed logout button behaviour

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -36,7 +36,7 @@ const HomePage = () => {
                     throw new Error('Network response was not ok');
                 }
                 setIsAuthenticated(false);
-                navigate('/login');
+                navigate('/');
             })
             .catch(error => {
                 console.error('There was a problem with the fetch operation:', error);
